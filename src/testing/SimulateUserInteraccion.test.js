5b6cc345cb2a0e40e6a5fa28e596ab0a2d717b49
benja27 <50673336+benja27@users.noreply.github.com>
2023-06-15 13:57:42 -0600

    add user interacion testing

--- src/testing/SimulateUserInteraccion.test.js
+++ src/testing/SimulateUserInteraccion.test.js
@@ -1,15 +1,31 @@
 import {render, fireEvent, screen} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "../App";
 import Navbar from "../components/Navbar";
+import Home from "../components/Home";
+import Calculator from "../components/calculator";
 
 
-test("test if elements inside calculator are render correctly", () => {
+test("test if home is displayed correctly when user clicks on home button", () => {
     render(
         <BrowserRouter>
           <Navbar/>
+          <Home/>
         </BrowserRouter>
     );
-    const buttonAc = screen.getByText(/Home/i);
-    expect(buttonAc).toBeInTheDocument();
+    const homeButton = screen.getByText(/Home/i);
+    
+    expect(screen.getByText('Welcome to our page')).toBeInTheDocument();
 });
+
+test("test if calculator is displayed correctly when user clicks on home calculator", () => {
+    render(
+        <BrowserRouter>
+          <Navbar/>
+          <Calculator/>
+        </BrowserRouter>
+    );
+    const homeButton = screen.getByText(/Home/i);
+    
+    expect(screen.getByText('Lets do some Math!')).toBeInTheDocument();
+});
+
